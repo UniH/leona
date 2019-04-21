fcc32e37d577b2820e1f3d1caa18075f67173e3d
Uni Huang <neverland1943@gmail.com>
2019-04-22 00:47:51 +0800

    bug fix

--- src/lambda/mailSender.js
+++ src/lambda/mailSender.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-async function handler() {
+export async function handler() {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
